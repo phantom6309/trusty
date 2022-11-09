8a27574a6dcf3358bc0bed2daa7ec0e893a09060
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2022-11-09 10:30:45 +0300

    Update tarotreading.py

--- tarot/tarotreading.py
+++ tarot/tarotreading.py
@@ -120,7 +120,7 @@ class TarotReading(commands.Cog):
         cards = sample((range(1, 78)), 5)
 
         embed = discord.Embed(
-            title="{} için Tarot falı".format(user.display_name),
+            title="Tarot falı {}".format(user.display_name),
             colour=discord.Colour(value=self.get_colour()),
         )
         embed.set_thumbnail(url=TAROT_CARDS[str(cards[-1])].card_img)
